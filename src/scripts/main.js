7536cba084bcf857cd04117a153b03d8cb030f50
Pieter Marx <126081747+TheRealPieterMarx@users.noreply.github.com>
2025-01-29 12:27:40 +0200

    Add files via upload

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+document.getElementById("footer-copyright").innerHTML =
+  "© " + new Date().getFullYear() + " Pieter Marx. All Rights Reserved.<br/>(Not really.. ;D)";
+
+function printResume() {
+  window.print();
+}
+
+function downloadResume() {
+  const resumePath = "../assets/docs/Pieter Marx Resume.pdf";
+  const link = document.createElement("a");
+  link.href = resumePath;
+  link.download = "Pieter Marx Resume.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+document.getElementById("download").addEventListener("click", function (event) {
+  event.preventDefault();
+  downloadResume();
+});
